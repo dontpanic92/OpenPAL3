19e76b649ed1ba205912d95207cb9c97b588fc6b
Li Shengqiu <lishengqiu.hit@gmail.com>
2020-02-19 17:07:47 +0800

    Add mv3 animation viewer

---
+++ opengb/src/loaders/mv3loader.rs
@@ -0,0 +1,254 @@
+use std::fs;
+use std::path::Path;
+use std::error::Error;
+use std::io::{Read, BufReader};
+use byteorder::{LittleEndian, ReadBytesExt};
+
+#[derive(Debug)]
+pub struct Mv3Texture {
+    pub unknown: Vec<u8>, // size: 68
+    pub names: Vec<Vec<u8>>,
+}
+
+#[derive(Debug)]
+pub struct Mv3Vertex {
+    pub x: i16,
+    pub y: i16,
+    pub z: i16,
+    pub normal_phi: i8,
+    pub normal_theta: u8,
+}
+
+#[derive(Debug)]
+pub struct Mv3Frame {
+    pub timestamp: u32,
+    pub vertices: Vec<Mv3Vertex>,
+}
+
+#[derive(Debug)]
+pub struct Mv3Triangle {
+    pub indices: [u16; 3],
+    pub texcoord_indices: [u16; 3],
+}
+
+#[derive(Debug)]
+pub struct Mv3UnknownDataInMesh {
+    pub u: u16,
+    pub v: u16,
+}
+
+#[derive(Debug)]
+pub struct Mv3Mesh {
+    pub unknown: u32,
+    pub triangle_count: u32,
+    pub triangles: Vec<Mv3Triangle>,
+    pub unknown_data_count: u32,
+    pub unknown_data: Vec<Mv3UnknownDataInMesh>,
+}
+
+#[derive(Debug)]
+pub struct Mv3TexCoord {
+    pub u: f32,
+    pub v: f32,
+}
+
+#[derive(Debug)]
+pub struct Mv3Model {
+    pub unknown: Vec<u8>, // size: 64
+    pub vertex_per_frame: u32,
+    pub aabb_min: [f32; 3],
+    pub aabb_max: [f32; 3],
+    pub frame_count: u32,
+    pub frames: Vec<Mv3Frame>,
+    pub texcoord_count: u32,
+    pub texcoords: Vec<Mv3TexCoord>, // size: 8 * unknown2_data_count
+    pub mesh_count: u32,
+    pub meshes: Vec<Mv3Mesh>,
+}
+
+#[derive(Debug)]
+pub struct Mv3File {
+    pub magic: [u8; 4],
+    pub unknown_dw: u32,
+    pub unknown_dw2: u32,
+    pub texture_count: u32,
+    pub unknown_data_count: u32,
+    pub model_count: u32,
+    pub action_count: u32,
+    pub action_desc: Vec<[u8; 20]>,
+    pub unknown_data: Vec<Vec<u8>>,
+    pub textures: Vec<Mv3Texture>,
+    pub models: Vec<Mv3Model>,
+}
+
+pub fn mv3_load_from_file<P: AsRef<Path>>(path: P) -> Result<Mv3File, Box<dyn Error>> {
+    let mut reader = BufReader::new(fs::File::open(path)?);
+    let mut magic = [0u8; 4];
+    reader.read_exact(&mut magic)?;
+
+    match magic {
+        [0x4d, 0x56, 0x33, 0x00] => (), // "MV3\0"
+        _ => panic!("Not a mv3 file"),
+    }
+
+    let unknown_dw = reader.read_u32::<LittleEndian>()?;
+    let unknown_dw2 = reader.read_u32::<LittleEndian>()?;
+    let texture_count = reader.read_u32::<LittleEndian>()?;
+    let unknown_data_count = reader.read_u32::<LittleEndian>()?;
+    let model_count = reader.read_u32::<LittleEndian>()?;
+    let action_count = reader.read_u32::<LittleEndian>()?;
+
+    let mut action_desc = vec![];
+    for _i in 0..action_count {
+        let mut buf = [0; 20];
+        reader.read_exact(&mut buf)?;
+        action_desc.push(buf);
+    }
+
+    let mut unknown_data = vec![];
+    println!("unknown_data_count {}", unknown_data_count);
+    for _i in 0..unknown_data_count {
+        let _buf = read_vec(&mut reader, 64)?;
+        reader.read_u32::<LittleEndian>()?;
+        let count = reader.read_u32::<LittleEndian>()?;
+        for _j in 0..count {
+            read_vec(&mut reader, 68)?;
+        }
+    }
+
+    let mut textures = vec![];
+    for _i in 0..texture_count {
+        let texture = {
+            let buf = read_vec(&mut reader, 68)?;
+            let mut names = vec![];
+
+            for _j in 0..4 {
+                let name_length = reader.read_u32::<LittleEndian>()?;
+                
+                let name = if name_length > 0 {
+                    read_vec(&mut reader, name_length as usize)?
+                } else {
+                    vec![]
+                };
+
+                names.push(name);
+            }
+
+            Mv3Texture {
+                unknown: buf,
+                names,
+            }
+        };
+
+        textures.push(texture);
+    }
+
+    let mut models = vec![];
+    for _i in 0..model_count {
+        let model = read_mv3_model(&mut reader)?;
+        models.push(model);
+    }
+
+    Ok(Mv3File {
+        magic,
+        unknown_dw,
+        unknown_dw2,
+        texture_count,
+        unknown_data_count,
+        model_count,
+        action_count,
+        action_desc,
+        unknown_data,
+        textures,
+        models,
+    })
+}
+
+fn read_mv3_model(reader: &mut dyn Read) -> Result<Mv3Model, Box<dyn Error>> {
+    let unknown = read_vec(reader, 64)?;
+    let vertex_per_frame = reader.read_u32::<LittleEndian>()?;
+    let mut aabb_min = [0f32; 3];
+    let mut aabb_max = [0f32; 3];
+    reader.read_f32_into::<LittleEndian>(&mut aabb_min)?;
+    reader.read_f32_into::<LittleEndian>(&mut aabb_max)?;
+    let frame_count = reader.read_u32::<LittleEndian>()?;
+    let mut frames = vec![];
+    for _i in 0..frame_count {
+        let timestamp = reader.read_u32::<LittleEndian>()?;
+        let mut vertices = vec![];
+        for _j in 0..vertex_per_frame {
+            let x = reader.read_i16::<LittleEndian>()?;
+            let y = reader.read_i16::<LittleEndian>()?;
+            let z = reader.read_i16::<LittleEndian>()?;
+            let normal_phi = reader.read_i8()?;
+            let normal_theta = reader.read_u8()?;
+            vertices.push(Mv3Vertex { x, y, z, normal_phi, normal_theta });
+        }
+        frames.push(Mv3Frame { timestamp, vertices});
+    }
+
+    let texcoord_count = reader.read_u32::<LittleEndian>()?;
+    let mut texcoords = vec![];
+    
+    for i in 0..texcoord_count {
+        let u = reader.read_f32::<LittleEndian>()?;
+        let v = reader.read_f32::<LittleEndian>()?;
+        texcoords.push(Mv3TexCoord { u, v });
+    }
+
+    let mesh_count = reader.read_u32::<LittleEndian>()?;
+    let mut meshes = vec![];
+    for _i in 0..mesh_count {
+        meshes.push(read_mv3_mesh(reader)?);
+    }
+
+
+    Ok(Mv3Model {
+        unknown,
+        vertex_per_frame,
+        aabb_min,
+        aabb_max,
+        frame_count,
+        frames,
+        texcoord_count,
+        texcoords,
+        mesh_count,
+        meshes,
+    })
+}
+
+fn read_mv3_mesh(reader: &mut dyn Read) -> Result<Mv3Mesh, Box<dyn Error>> {
+    let unknown = reader.read_u32::<LittleEndian>()?;
+    let triangle_count = reader.read_u32::<LittleEndian>()?;
+    let mut triangles = vec![];
+    for _i in 0..triangle_count {
+        let mut indices = [0u16; 3];
+        let mut texcoord_indices = [0u16; 3];
+        reader.read_u16_into::<LittleEndian>(&mut indices)?;
+        reader.read_u16_into::<LittleEndian>(&mut texcoord_indices)?;
+
+        triangles.push(Mv3Triangle { indices, texcoord_indices })
+    }
+
+    let unknown_data_count = reader.read_u32::<LittleEndian>()?;
+    let mut unknown_data = vec![];
+    for _i in 0..unknown_data_count {
+        let u = reader.read_u16::<LittleEndian>()?;
+        let v = reader.read_u16::<LittleEndian>()?;
+        unknown_data.push(Mv3UnknownDataInMesh { u, v })
+    }
+
+    Ok(Mv3Mesh {
+        unknown,
+        triangle_count,
+        triangles,
+        unknown_data_count,
+        unknown_data,
+    })
+}
+
+fn read_vec(reader: &mut dyn Read, size: usize) -> Result<Vec<u8>, Box<dyn Error>> {
+    let mut buf = vec![0u8; size];
+    reader.read_exact(&mut buf.as_mut_slice())?;
+    Ok(buf)
+}
--- tools/mv3viewer/src/main.rs
+++ tools/mv3viewer/src/main.rs
@@ -1,3 +1,36 @@
+mod scene;
+
+use radiance::application;
+use radiance::scene::CoreScene;
+use nfd::Response;
+
+struct ApplicationCallbacks {
+    pub path: String,
+}
+
+impl application::ApplicationCallbacks for ApplicationCallbacks {
+    fn on_initialized<T: application::ApplicationCallbacks>(&mut self, app: &mut application::Application<T>) {
+        app.engine_mut().load_scene(CoreScene::new(scene::ModelViewerScene { path: self.path.clone() }));
+    }
+
+    fn on_updated<T: application::ApplicationCallbacks>(&mut self, app: &mut application::Application<T>, delta_sec: f32) {
+        let title = format!("MV3 Animation Viewer - OpenPAL3 Tools - FPS: {}", 1./delta_sec);
+        app.set_title(&title);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result = nfd::open_file_dialog(None, None).unwrap_or_else(|e| {
+        panic!(e);
+    });
+  
+    let path = match result {
+        Response::Okay(file_path) => file_path,
+        Response::OkayMultiple(files) => String::from(&files[0]),
+        Response::Cancel => std::process::exit(0),
+    };
+
+    let mut application = application::Application::new(ApplicationCallbacks { path });
+    application.initialize();
+    application.run();
 }
