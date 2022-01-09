c2000d6f7c3964e79e36bea2074437597be09fbc
chrox <chrox.huang@gmail.com>
2022-01-10 00:17:26 +0800

    Fix font texture building on macOS see https://github.com/dontpanic92/imgui-rs-vulkan-renderer/pull/2

--- radiance/src/rendering/vulkan/imgui.rs
+++ radiance/src/rendering/vulkan/imgui.rs
@@ -38,6 +38,23 @@ impl ImguiRenderer {
                 Allocator::new(&allocator_create_info).unwrap()
             };
 
+            // Texture width desired by user before building the atlas.
+            // Must be a power-of-two. If you have many glyphs and your graphics API has texture size
+            // restrictions, you may want to increase texture width to decrease the height.
+            // For example, Apple's Metal API (MTLTextureDescriptor) only supports
+            // a maximum size of 16384 (128x128) for texture dimension on M1 devices.
+            // We are defining the max width to be the max image dimension size get from device
+            // properties here to be safe.
+            if cfg!(target_os = "macos") {
+                let device_properties = unsafe {
+                    instance
+                        .vk_instance()
+                        .get_physical_device_properties(physical_device)
+                };
+                context.context_mut().fonts().tex_desired_width =
+                    device_properties.limits.max_image_dimension2_d as i32;
+            }
+
             let options = Some(Options {
                 in_flight_frames: 1,
                 enable_depth_test: true,
