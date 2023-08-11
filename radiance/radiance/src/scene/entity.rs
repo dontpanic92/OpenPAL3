dontpanic92/OpenPAL3

commit 34684efd40130df472152fe80df361d34de7f0f8

Improve scene perf

radiance/radiance/src/scene/entity.rs
@@ -1,5 +1,4 @@
 use crosscom::ComRc;
-use dashmap::DashMap;
 use uuid::Uuid;
 
 use crate::comdef::{IComponent, IComponentContainerImpl, IEntity, IEntityImpl};
@@ -7,11 +6,12 @@ use crate::math::{Mat44, Transform};
 use crate::rendering::RenderingComponent;
 use crate::ComObject_Entity;
 use std::cell::{Ref, RefCell, RefMut};
+use std::collections::HashMap;
 use std::rc::Rc;
 
 pub struct CoreEntity {
     transform: Rc<RefCell<Transform>>,
-    components: DashMap<Uuid, ComRc<IComponent>>,
+    components: RefCell<HashMap<Uuid, ComRc<IComponent>>>,
     props: RefCell<CoreEntityProps>,
 }
 
@@ -38,7 +38,7 @@ impl CoreEntity {
     pub fn new(name: String, visible: bool) -> Self {
         Self {
             transform: Rc::new(RefCell::new(Transform::new())),
-            components: DashMap::new(),
+            components: RefCell::new(HashMap::new()),
             props: RefCell::new(CoreEntityProps {
                 name,
                 world_transform: Transform::new(),
@@ -70,17 +70,21 @@ impl CoreEntity {
 impl IComponentContainerImpl for CoreEntity {
     fn add_component(&self, uuid: uuid::Uuid, component: crosscom::ComRc<IComponent>) -> () {
         component.on_loading();
-        self.components.insert(uuid, component);
+        self.components.borrow_mut().insert(uuid, component);
     }
 
     fn get_component(&self, uuid: uuid::Uuid) -> Option<ComRc<IComponent>> {
         self.components
+            .borrow()
             .get(&uuid)
-            .and_then(|c| Some(c.value().clone()))
+            .and_then(|c| Some(c.clone()))
     }
 
     fn remove_component(&self, uuid: uuid::Uuid) -> Option<ComRc<IComponent>> {
-        self.components.remove(&uuid).and_then(|c| Some(c.1))
+        self.components
+            .borrow_mut()
+            .remove(&uuid)
+            .and_then(|c| Some(c))
     }
 }
 
@@ -109,7 +113,7 @@ impl IEntityImpl for CoreEntity {
         }
 
         self.props_mut().children.clear();
-        self.components.clear();
+        self.components.borrow_mut().clear();
     }
 
     fn update(&self, delta_sec: f32) -> crosscom::Void {
@@ -117,8 +121,8 @@ impl IEntityImpl for CoreEntity {
             e.update(delta_sec);
         }
 
-        for c in self.components.clone() {
-            c.1.on_updating(delta_sec);
+        for c in self.components.borrow().values().clone() {
+            c.on_updating(delta_sec);
         }
     }
 
@@ -138,7 +142,7 @@ impl IEntityImpl for CoreEntity {
             self.transform.borrow().matrix(),
         ));
 
-        for e in props.children.clone() {
+        for e in &props.children {
             e.update_world_transform(&props.world_transform);
         }
     }

yaobow/shared/src/openpal4/director.rs
@@ -1,7 +1,6 @@
 use std::{cell::RefCell, rc::Rc};
 
 use crosscom::ComRc;
-use ffmpeg::codec::audio;
 use radiance::{
     audio::AudioEngine,
     comdef::{IDirectorImpl, ISceneManager},

yaobow/yaobow_editor/Cargo.toml
@@ -18,6 +18,7 @@ image = "0.23.0"
 imgui = { version="0.10", features = ["docking", "tables-api"] }
 lazy_static = "1.2.0"
 log = { version = "0.4", features = ["max_level_debug", "release_max_level_debug"] }
+lru = { version = "0.7.1", features=["nightly"] }
 mini-fs = { git = "https://github.com/dontpanic92/mini-fs/" }
 native-dialog = "0.6.3"
 radiance = { path = "../../radiance/radiance" }

yaobow/yaobow_editor/src/directors/main_director.rs
@@ -25,6 +25,7 @@ use std::{
 pub struct DevToolsDirector {
     asset_mgr: Rc<AssetManager>,
     content_tabs: RefCell<ContentTabs>,
+    cache: RefCell<lru::LruCache<String, Vec<Entry>>>,
 }
 
 ComObject_DevToolsDirector!(super::DevToolsDirector);
@@ -49,6 +50,7 @@ impl DevToolsDirector {
                 texture_resolver,
             )),
             asset_mgr,
+            cache: RefCell::new(lru::LruCache::new(20)),
         })
     }
 
@@ -120,16 +122,23 @@ impl DevToolsDirector {
     }
 
     fn get_entries<P: AsRef<Path>>(&self, path: P) -> Vec<Entry> {
-        let entries: Entries = self.asset_mgr.vfs().entries(path.as_ref()).unwrap();
-        let mut entries: Vec<Entry> = entries.map(|e| e.unwrap()).collect();
-        entries.sort_by(|a, b| match (a.kind, b.kind) {
-            (EntryKind::Dir, EntryKind::Dir) => a.name.cmp(&b.name),
-            (EntryKind::File, EntryKind::File) => a.name.cmp(&b.name),
-            (EntryKind::Dir, EntryKind::File) => Ordering::Less,
-            (EntryKind::File, EntryKind::Dir) => Ordering::Greater,
-        });
+        let key = path.as_ref().to_string_lossy().to_string();
+        self.cache
+            .borrow_mut()
+            .get_or_insert(key, || {
+                let entries: Entries = self.asset_mgr.vfs().entries(path.as_ref()).unwrap();
+                let mut entries: Vec<Entry> = entries.map(|e| e.unwrap()).collect();
+                entries.sort_by(|a, b| match (a.kind, b.kind) {
+                    (EntryKind::Dir, EntryKind::Dir) => a.name.cmp(&b.name),
+                    (EntryKind::File, EntryKind::File) => a.name.cmp(&b.name),
+                    (EntryKind::Dir, EntryKind::File) => Ordering::Less,
+                    (EntryKind::File, EntryKind::Dir) => Ordering::Greater,
+                });
 
-        entries
+                entries
+            })
+            .unwrap()
+            .clone()
     }
 }
 

commit 67f5f28302136d3394e2ba7fc5d9a23044da7d9c

fix already borrowed error
Crash message:
  panicked at 'already borrowed: BorrowMutError', radiance\radiance\src\scene\entity.rs:73:33

The @components will borrow_mut() during iterating under borrow(), which
causes the crash.  Clone it before iterating to fix this crash.

Fixes: 34684ef ("Improve scene perf")
Signed-off-by: ryan <mr.iridescent.rsy@hotmail.com>

radiance/radiance/src/scene/entity.rs
@@ -121,7 +121,8 @@ impl IEntityImpl for CoreEntity {
             e.update(delta_sec);
         }
 
-        for c in self.components.borrow().values().clone() {
+        let components = self.components.borrow().clone();
+        for c in components.values() {
             c.on_updating(delta_sec);
         }
     }