8eb19930154f373ee62c5f83f6fb0e0ccd2fabb4
Li Shengqiu <lishengqiu.hit@gmail.com>
2023-12-16 16:29:07 +0800

    swd5: test run swd5 scripts

--- yaobow/shared/src/openswd5/mod.rs
+++ yaobow/shared/src/openswd5/mod.rs
@@ -1,9 +1,5 @@
-use std::io::Cursor;
-
-use binrw::BinRead;
+use encoding::{DecoderTrap, Encoding};
 use fileformats::{binrw::BinReaderExt, c00::C00};
-
-use crate::scripting::lua50::chunk::LuaChunk;
 
 pub fn test() {
     let data =
@@ -20,11 +16,48 @@ pub fn test() {
         .decompress(&c00.data, c00.header.original_size as usize)
         .unwrap();
 
-    let chunk = LuaChunk::read(&mut Cursor::new(out)).unwrap();
-    println!("{:?}", chunk);
+    unsafe {
+        let state = lua50_32_sys::lua_open();
 
-    /*let lua = Lua::new();
-    lua.context(|ctx| {
-        ctx.load(&output_bytes).exec().unwrap();
-    })*/
+        lua50_32_sys::luaopen_base(state);
+        lua50_32_sys::luaopen_table(state);
+        lua50_32_sys::luaopen_io(state);
+        lua50_32_sys::luaopen_string(state);
+        lua50_32_sys::luaopen_math(state);
+        lua50_32_sys::luaopen_debug(state);
+        lua50_32_sys::luaopen_loadlib(state);
+
+        let ret = lua50_32_sys::luaL_loadbuffer(
+            state,
+            out.as_ptr() as *const i8,
+            out.len(),
+            b"main\0".as_ptr() as *const i8,
+        );
+
+        println!("ret: {}", ret);
+
+        let call_ret = lua50_32_sys::lcall(state, 0, 0);
+        println!("call_ret: {}", call_ret);
+        if call_ret > 0 {
+            let s = lua50_32_sys::lua_tostring(state, -1);
+            let str = std::ffi::CStr::from_ptr(s);
+            println!("error: {}", str.to_str().unwrap());
+        }
+
+        lua50_32_sys::getglobal(state, b"initiatelua\0".as_ptr() as *const i8);
+
+        let call_ret = lua50_32_sys::lcall(state, 0, 0);
+        println!("call_ret2: {}", call_ret);
+
+        if call_ret > 0 {
+            let s = lua50_32_sys::lua_tostring(state, -1);
+            let str = std::ffi::CStr::from_ptr(s);
+            let str = encoding::all::BIG5_2003
+                .decode(str.to_bytes(), DecoderTrap::Ignore)
+                .unwrap();
+            println!("error: {}", str);
+        }
+
+        lua50_32_sys::lua_close(state);
+    }
 }
